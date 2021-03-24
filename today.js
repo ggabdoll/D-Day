f4f5ab17432bd97765209f21dd7881d82b782934
ggabdoll <ggabdoll0327@naver.com>
2021-03-25 01:01:08 +0900

    21.3.25

---
+++ today.js
@@ -0,0 +1,20 @@
+const today = document.querySelector(".js-today",".today__text");
+
+function getDate(){
+    const tod = new Date();
+    const month = tod.getMonth();
+    const day = tod.getDate();
+    const todtext =`${month}.${day < 10 ? `0${day}` : day}`;
+    today.innerHTML = todtext;
+    return;
+}
+
+
+
+function init(){
+    getDate();
+    setInterval(getDate,100000);
+    return;
+}
+
+init();
