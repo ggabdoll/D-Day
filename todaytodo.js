4ea8a9e4012fe6ceee5e9de3b37d9f7888f74653
ggabdoll <ggabdoll0327@naver.com>
2021-03-26 01:17:51 +0900

    weather, todo start

---
+++ todaytodo.js
@@ -0,0 +1,14 @@
+const toTodoForm = document.querySelector(".js-todayTodo"),
+    toTodoInput = toTodoForm.querySelector("input"),
+    todayTodoList = document.querySelector("js-todayTOdoList");
+
+
+
+
+
+
+function init(){
+
+}
+
+init();
---
+++ weathers.js
@@ -0,0 +1,11 @@
+const weathers = document.querySelector(".js-weathers");
+const WAPI_KEY = "caf2229848ccad01293765cd4e34d565";
+
+
+
+
+function init(){
+
+}
+
+init();
