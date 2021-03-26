e8ecf0fd400ba519abfaaa3a1ae098fa1cd49582
ggabdoll <ggabdoll0327@naver.com>
2021-03-26 18:06:13 +0900

    21.3.26

--- todaytodo.js
+++ todaytodo.js
@@ -2,7 +2,30 @@ const toTodoForm = document.querySelector(".js-todayTodo"),
     toTodoInput = toTodoForm.querySelector("input"),
     todayTodoList = document.querySelector("js-todayTOdoList");
 
+let toTodos = [];   
 
+function deleteTotodo(event){
+    const butt = event.target;
+    const toli = butt.parentNode;
+    todayTodoList.removeChild(toli);
+    const cleantodo = toTodos.filter(function(toToDo){
+        return toTodos.id !== parseInt(toli.id);
+    });
+    toTodos = cleantodo;
+    
+
+}
+
+    function paintToTodo(text){
+    const toli = document.createElement("toli");
+}
+
+
+function toDoSubmit(event){
+    event.preventDefault();
+    const curValu = toTodoInput.value;
+
+}
 
 
 
