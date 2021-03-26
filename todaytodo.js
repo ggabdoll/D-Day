d23bd001b14494ca0b73974ae288d07abb8f372f
ggabdoll <ggabdoll0327@naver.com>
2021-03-26 18:49:17 +0900

    Today to do

--- todaytodo.js
+++ todaytodo.js
@@ -2,6 +2,8 @@ const toTodoForm = document.querySelector(".js-todayTodo"),
     toTodoInput = toTodoForm.querySelector("input"),
     todayTodoList = document.querySelector("js-todayTOdoList");
 
+const TOTODOS_LS = 'toTodos';
+
 let toTodos = [];   
 
 function deleteTotodo(event){
@@ -12,26 +14,62 @@ function deleteTotodo(event){
         return toTodos.id !== parseInt(toli.id);
     });
     toTodos = cleantodo;
-    
+    saveTotodos();
 
+
+
+}
+
+function saveTotodos(){
+    localStorage.setItem(TOTODOS_LS, JSON.stringify(toTodos));
 }
 
     function paintToTodo(text){
     const toli = document.createElement("toli");
+    const delButt = document.createElement("button");
+    const span = document.createElement("span");
+    const newId = toTodos.length+1;
+    delButt.innerHTML = "😀";
+    delButt.className = "toTodo_butt";
+    delButt.addEventListener("click", deleteTotodo);
+    span.innerText = text;
+    toli.appendChild(delButt);
+    toli.appendChild(span);
+    toli.id = newId;
+    todayTodoList.appendChild(toli);
+    const toDoObj = {
+        text: text,
+        id: newId
+    };
+    toTodos.push(toDoObj);
+    saveTotodos();
 }
 
 
 function toDoSubmit(event){
     event.preventDefault();
     const curValu = toTodoInput.value;
+    paintToTodo(curValu);
+    toTodoInput.value = "";
 
+}
+
+function loadTotodos(){
+    const loadedTotoDos = localStorage.getItem(TOTODOS_LS);
+    if(loadTotodos !== null){
+        const parsedTotodos = JSON.parse(loadedTotoDos);
+        parsedTotodo.forEach(function(toTodos) {
+            paintToTodo(toToDos.text);
+        });
+    }
 }
 
 
 
 
 function init(){
-
+    loadTotodos();
+    toTodoForm.addEventListener("submit", toDoSubmit)
 }
 
 init();
